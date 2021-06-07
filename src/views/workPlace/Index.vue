8b4bafee83027f6f8d6b0350223fb5c95cd83a07
Anxure <505959201@qq.com>
2021-06-07 17:26:07 +0800

    feat：新增工作台页面

--- src/views/workPlace/Index.vue
+++ src/views/workPlace/Index.vue
@@ -1,45 +1,31 @@
 
 <template>
-  <div>
-    <ul>
-      <li>{{name}}</li>
-      <li>{{state}}</li>
-      <li></li>
-      <li></li>
-    </ul>
-    <a-button @click="changeUserInfo"></a-button>
-    <test-props :name="name" @changeNameFromChild="changeNameFromChild"></test-props>
+  <div class="workplace">
+    <div class="workplace-header">
+      <a-header></a-header>
+    </div>
   </div>
 </template>
 
-<script>
-import { reactive, toRefs, ref } from 'vue'
-import TestProps from '../../components/TestProps.vue'
-export default {
-  components: {TestProps},
-  setup () {
-    const userInfo = reactive({name: 'xulian', age: 21})
-    const state = ref(false)
-    console.log('%cIndex.vue line:17 object', 'color: #007acc;', userInfo);
-    console.log('%cIndex.vue line:17 object', 'color: #007acc;', state);
-    function changeUserInfo() {
-      userInfo.name = '修改后的值'
-      state.value = true
-    }
-    function changeNameFromChild(val) {
-      userInfo.name = val
-    }
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import AHeader from './components/Aheader.vue'
+export default defineComponent({
+  components: {
+    AHeader
+  },
+  setup(props) {
+    console.log(props)
+    const loading = ref<boolean>(true)
+    setTimeout(() => {
+      loading.value = false
+    }, 1000)
     return {
-      changeUserInfo,
-      state,
-      changeNameFromChild,
-      ...toRefs(userInfo)
-
+      loading
     }
   }
-}
+});
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
 </style>
---
+++ src/views/workPlace/components/Aheader.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-card>
+    <div class="flex align-item-center">
+      <a-avatar class="user-avatar" :size="72" :src="headerImg"></a-avatar>
+      <div class="user-text">
+        <h3>早安，Anxure，开始您一天的工作吧！</h3>
+        <span>今日天气：多云， 温度：19℃ - 35℃</span>
+      </div>
+    </div>
+  </a-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import headerImg from '@/assets/image/avatar.png';
+export default defineComponent({
+  setup() {
+    return {
+      headerImg
+    };
+  }
+});
+</script>
+
+<style scoped lang="less">
+.user-text{
+  margin-left: 20px;
+  h3{
+    font-weight: bold;
+  }
+}
+</style>
