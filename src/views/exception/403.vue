02f94c4c055a7fae5978b76a36ebdb7e9ec728ee
Anxure <505959291@qq.com>
2021-06-08 18:12:26 +0800

    feat: 新增403、404页面

--- src/views/exception/403.vue
+++ src/views/exception/403.vue
@@ -1,19 +1,40 @@
 <template>
-  <div>
-    403
+  <div class="error-content flex align-item-center flex-center column">
+    <div class="error-icon">
+      <svg class="icon" aria-hidden="true" style="font-size: 200px">
+        <use xlink:href="#icon-quanxian"></use>
+      </svg>
+    </div>
+    <div class="title">抱歉，你当前没有权限访问</div>
+    <a-button class="t-3" type="primary" :loading="loading" @click="repeatLogin">返回首页</a-button>
   </div>
 </template>
 
 <script lang="ts">
+import { useRouter } from 'vue-router'
 export default {
-  setup () {
-
-
-    return {}
+  setup() {
+    const router = useRouter()
+    function repeatLogin() {
+      router.push('/workplace')
+    }
+    return {
+      repeatLogin
+    };
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-
+.error-content{
+  width: 100%;
+  height: 100%;
+  background-color:#FFF;
+  .title {
+      text-align: center;
+      font-size: 16px;
+      color: #d80637;
+      font-weight: 600;
+    }
+}
 </style>
--- src/views/exception/404.vue
+++ src/views/exception/404.vue
@@ -1,19 +1,39 @@
 <template>
-  <div>
-    404
+  <div class="error-content flex align-item-center flex-center column">
+    <div class="error-icon">
+      <svg class="icon" aria-hidden="true" style="font-size: 200px">
+        <use xlink:href="#icon-404"></use>
+      </svg>
+    </div>
+    <div class="title">抱歉，页面消失了</div>
+    <a-button class="t-3" type="primary" :loading="loading" @click="repeatLogin">返回首页</a-button>
   </div>
 </template>
 
 <script lang="ts">
+import { useRouter } from 'vue-router'
 export default {
-  setup () {
-
-
-    return {}
+  setup() {
+    const router = useRouter()
+    function repeatLogin() {
+      router.push('/workplace')
+    }
+    return {
+      repeatLogin
+    };
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-
+.error-content{
+  width: 100%;
+  height: 100%;
+  background-color:#FFF;
+  .title {
+      text-align: center;
+      font-size: 16px;
+      font-weight: 600;
+    }
+}
 </style>
