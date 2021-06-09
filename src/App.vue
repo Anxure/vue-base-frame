6863038d0b608ba7207f5bd71e1e760939ada0a7
Anxure <505959291@qq.com>
2021-06-09 11:19:29 +0800

    feat: 新增ant-design-vue国际化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <router-view />
+  <a-config-provider :locale="locale">
+    <router-view />
+  </a-config-provider>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+export default defineComponent({
+  setup() {
+    return {
+      locale: zhCN
+    };
+  }
+});
+</script>
 
 <style>
 #app {
