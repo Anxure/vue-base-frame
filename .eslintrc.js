d16225b89ecb420ffa1e74dec3703a59924eac7e
Anxure <505959291@qq.com>
2021-06-21 18:03:33 +0800

    style: 修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,24 +4,26 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
     ecmaVersion: 2020,
     parser: '@typescript-eslint/parser'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'quotes': [2, 'single'], // 变量必须使用单引号
     // 'no-explicit-any': 'off',
     'no-unused-vars': [2, { 'vars': 'all', 'args': 'after-used' }],
     '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
+    'no-extra-boolean-cast': 0,
+    '@typescript-eslint/no-non-null-assertion': 'off',
     'prettier/prettier': [
       'off',
       {
