96e9e1763737ba76a031b10336659d0dfebacb6e
Anxure <505959291@qq.com>
2021-07-05 09:23:49 +0800

    fix: 修复ts类型

--- src/components/multiTab/Index.vue
+++ src/components/multiTab/Index.vue
@@ -2,30 +2,39 @@
   <div class="multi-tab-box">
     <a-tabs v-model:activeKey="activeKey" type="editable-card" @edit="onEdit" hideAdd @tabClick="tabClick" :tabBarStyle="{ background: '#FFF', margin: 0, paddingLeft: '16px', paddingTop: '1px' }">
       <a-tab-pane v-for="tab in pages" :key="tab.fullPath" :tab="tab.meta.title" :closable="pages.length > 1">
-      <a-dropdown :trigger="['contextmenu']"></a-dropdown>
       </a-tab-pane>
     </a-tabs>
   </div>
 </template>
 
-<script>
-import { reactive, toRefs, onMounted } from 'vue';
-import { onBeforeRouteUpdate, useRoute, useRouter } from 'vue-router';
+<script lang="ts">
+import { reactive, toRefs, onMounted} from 'vue';
+import { onBeforeRouteUpdate, RouteLocationNormalized, useRoute, useRouter,RouteMeta } from 'vue-router';
 export default {
   setup() {
-    const tabOptions = reactive({
+    interface Page {
+      meta: RouteMeta,
+      fullPath: string
+    }
+
+    interface TabOptions {
+      pages: Array<Page>
+      fullListPath: Array<string>
+      activeKey: string
+    }
+    const tabOptions= reactive<TabOptions>({
       pages: [],
       fullListPath: [],
       activeKey: ''
     });
     const route = useRoute();
     const router = useRouter();
-    createOption(route)
+    createOption(route);
     onBeforeRouteUpdate((to) => {
       tabOptions.activeKey = to.fullPath;
-      createOption(to)
+      createOption(to);
     });
-    function createOption(to) {
+    function createOption(to: RouteLocationNormalized) {
       if (tabOptions.fullListPath.indexOf(to.fullPath) < 0) {
         tabOptions.pages.push({ meta: to.meta, fullPath: to.fullPath });
         tabOptions.fullListPath.push(to.fullPath);
@@ -33,20 +42,20 @@ export default {
     }
     function selectedLastPath() {
       tabOptions.activeKey = tabOptions.fullListPath[tabOptions.fullListPath.length - 1];
-      router.push({path: tabOptions.activeKey })
+      router.push({ path: tabOptions.activeKey });
 
       console.log(tabOptions);
     }
     onMounted(() => {
       selectedLastPath();
     });
-    function onEdit(targetKey) {
+    function onEdit(targetKey: string) {
       remove(targetKey);
     }
-    function tabClick (targetKey) {
-      router.push({path: targetKey })
+    function tabClick(targetKey: string) {
+      router.push({ path: targetKey });
     }
-    function remove(targetKey) {
+    function remove(targetKey: string) {
       tabOptions.pages = tabOptions.pages.filter((page) => page.fullPath !== targetKey);
       tabOptions.fullListPath = tabOptions.fullListPath.filter((list) => list !== targetKey);
       // 判断当前标签是否关闭，若关闭则跳转到最后一个还存在的标签页
