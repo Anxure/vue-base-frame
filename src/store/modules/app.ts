399cf203b05c9bd77b77d4c960b9e2cf0d13fa62
Anxure <505959291@qq.com>
2022-06-06 16:01:26 +0800

    feat: 新增title配置

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -1,13 +1,15 @@
 export interface AppState {
   collapsed: boolean,
-  theme: string
+  theme: string,
+  title: string
 }
 import { layoutSetting } from '@/config/globalSetting'
 export default {
   namespaced: true,
   state: (): AppState => ({
     collapsed: false,
-    theme: layoutSetting.theme
+    theme: layoutSetting.theme,
+    title:layoutSetting.title
   }),
   mutations: {
     TOGGLECOLLAPSED(state: AppState) {
